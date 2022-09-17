cb878c1d8e8bef845d8f3a6efe99e4043c8f6fa7
KaiKai0714 <107627779+KaiKai0714@users.noreply.github.com>
2022-09-17 19:46:49 +0800

    Add files via upload

---
+++ json/menu.js
@@ -0,0 +1,15 @@
+function click(){
+    
+}
+
+var btn = document.getElementById("rfs");
+btn.addEventListener("click", click)
+
+var btn = document.getElementById("tetr");
+btn.addEventListener("click", click)
+
+var btn = document.getElementById("osu");
+btn.addEventListener("click", click)
+
+var btn = document.getElementById("card");
+btn.addEventListener("click", click)
