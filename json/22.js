19948aef5c4ec5a69a7010a02e1204fcc58beb9d
KaiKai0714 <107627779+KaiKai0714@users.noreply.github.com>
2022-09-19 21:55:34 +0800

    Update 22.js

--- json/22.js
+++ json/22.js
@@ -3,6 +3,42 @@ function click(){
     click.play();
 }
 
+function pon(){
+    const click = new Audio('../sound/碰.wav');
+    click.play();
+}
+
+function chi(){
+    const click = new Audio('../sound/吃.wav');
+    click.play();
+}
+
+function gun(){
+    const click = new Audio('../sound/槓.wav');
+    click.play();
+}
+
+function pe(){
+    const click = new Audio('../sound/拔北.wav');
+    click.play();
+}
+
+function lige(){
+    const click = new Audio('../sound/立直.wav');
+    click.play();
+}
+
+function lon(){
+    const click = new Audio('../sound/和.wav');
+    click.play();
+}
+
+function tsumo(){
+    const click = new Audio('../sound/自摸.wav');
+    click.play();
+}
+
+
 var btn = document.getElementById("click");
 btn.addEventListener("click", click);
 
