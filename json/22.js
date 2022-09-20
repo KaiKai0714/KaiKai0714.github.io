01e2c2575d5792d13e3b14e50f07d37e3de665d3
KaiKai0714 <107627779+KaiKai0714@users.noreply.github.com>
2022-09-20 21:04:06 +0800

    Update 22.js

--- json/22.js
+++ json/22.js
@@ -3,42 +3,64 @@ function click(){
     click.play();
 }
 
+var btn = document.getElementById("click");
+btn.addEventListener("click", click);
+
 function pon(){
     const pon = new Audio('../sound/碰.wav');
     pon.play();
 }
+
+var btn = document.getElementById("pon");
+btn.addEventListener("click", pon);
 
 function chi(){
     const chi = new Audio('../sound/吃.wav');
     chi.play();
 }
 
+var btn = document.getElementById("chi");
+btn.addEventListener("click", chi);
+
 function gun(){
     const gun = new Audio('../sound/槓.wav');
     gun.play();
 }
+
+var btn = document.getElementById("gun");
+btn.addEventListener("click", gun);
 
 function pe(){
     const pe = new Audio('../sound/拔北.wav');
     pe.play();
 }
 
+var btn = document.getElementById("pe");
+btn.addEventListener("click", pe);
+
 function lige(){
     const lige = new Audio('../sound/立直.wav');
     lige.play();
 }
+
+var btn = document.getElementById("lige");
+btn.addEventListener("click", lige);
 
 function lon(){
     const lon = new Audio('../sound/和.wav');
     lon.play();
 }
 
+var btn = document.getElementById("lon");
+btn.addEventListener("click", lon);
+
 function tsumo(){
     const tsumo = new Audio('../sound/自摸.wav');
     tsumo.play();
 }
 
+var btn = document.getElementById("tsumo");
+btn.addEventListener("click", tsumo);
 
-var btn = document.getElementById("click");
-btn.addEventListener("click", click);
+
 
