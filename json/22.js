3241e3a06b0fcb844e719ca8557e2953ea62b3d7
KaiKai0714 <107627779+KaiKai0714@users.noreply.github.com>
2022-09-25 01:39:29 +0800

    Update 22.js

--- json/22.js
+++ json/22.js
@@ -64,12 +64,16 @@ function rword(min, max){ //字牌
     var word = ["東","南","西","北","中","發","白"];
     return word[Math.round(Math.random()*(max-min)+min)]
 }
+function rhead(min, max){ //字牌
+    var word = ["萬","筒","條"];
+    return word[Math.round(Math.random()*(max-min)+min)]
+}
 
 var play = document.getElementById("play");
 play.addEventListener("click", function play(){                   
     var 萬c = {nb:rint(1,9),style:rstyle(1,2)}; var 筒c = {nb:rint(1,9),style:rstyle(1,2)}; 
     var 條c = {nb:rint(1,9),style:rstyle(1,2)}; var 字c = {nb:rword(0,6),style:rstyle(2,2)};
-    var 雀c = {nb:rint(1,9),w:rword(0,6),flower:rint(0,1),style:rstyle(2,2)}
+    var 雀c = {nb:rint(1,9),w:rword(0,6),flower:rint(0,1),wtt:rhead(0,2),style:rstyle(2,2)}
     var card = [萬c,筒c,條c,字c,雀c]
     for (let t=0; t<5; t++){
         if ((card[t])["style"] == "順子") {
@@ -85,7 +89,7 @@ play.addEventListener("click", function play(){
             else if (t == 2) {var 條 = [條c["nb"],條c["nb"],條c["nb"]]}
             else if (t == 3) {var 字 = [字c["nb"],字c["nb"],字c["nb"]]}
             else if (t == 4) {
-                if (雀c["flower"] == 0) {var 雀 = [`${雀c["nb"]}萬`,`${雀c["nb"]}萬`]}
+                if (雀c["flower"] == 0) {var 雀 = [`${雀c["nb"]}${雀c["wtt"]}`,`${雀c["nb"]}${雀c["wtt"]}`]}
                 else {var 雀 = [`${雀c["w"]}字`,`${雀c["w"]}字`]}
             }
         }
